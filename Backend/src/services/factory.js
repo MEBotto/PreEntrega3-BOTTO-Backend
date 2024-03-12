de5176b7a5064c9e94e96b7838ac8aa8bcccd883
Mariano Botto <marianobotto@gmail.com>
2024-03-11 23:49:06 -0300

    Commit PreEntrega3

---
+++ Backend/src/services/factory.js
@@ -0,0 +1,4 @@
+import CartDAO from "./dao/mongo/cart.dao.js";
+import ProductDAO from "./dao/mongo/product.dao.js";
+import MessageDAO from "./dao/mongo/message.dao.js";
+import UserDAO from "./dao/mongo/users.dao.js";
